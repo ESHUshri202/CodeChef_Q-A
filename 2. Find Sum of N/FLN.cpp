0fddf2cfd9d49db85e43d9b7c1ad6161b180c2c4
Achintya Shrivastava <85828877+ESHUshri202@users.noreply.github.com>
2023-07-14 20:53:34 +0530

    Add files via upload

---
+++ 2. Find Sum of N/FLN.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n, sum=0,i=1;
+    cin>>n;
+    while(i<=n){
+        sum=sum+i;
+        i=i+1;
+    }
+    cout<<sum<<endl;
+    
+}
